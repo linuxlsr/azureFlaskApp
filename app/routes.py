df469d4e5b656ea0e6f3f5b39103df475be2d282
Charles Harvey <soops@ucla.edu>
2020-06-18 08:40:36 -0700

    add templates code

--- app/routes.py
+++ app/routes.py
@@ -6,4 +6,23 @@ from app import app
 def index():
     user = {'username': 'Charles'}
     title = 'My Page'
-    return render_template('index.html', title=title, user=user)
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title=title, user=user, posts=posts)
+    # return '''
+    # <html>
+    #     <head>
+    #         <title>Home Page - Microblog</title>
+    #     <head>
+    #     <body>
+    #         <h1>Hello, ''' +user['username'] + '''!</h1>
+    #     </body>
+    # <html>'''
